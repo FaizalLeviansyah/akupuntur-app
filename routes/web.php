9204c51f6b923f1843740c1a727fd6ea700447ad
Levi <91838778+FaizalLeviansyah@users.noreply.github.com>
2026-01-26 11:00:54 +0700

    Add medical records routes for patients
    Introduces routes for creating, storing, and viewing medical records associated with patients, handled by the MedicalRecordController.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\MedicalRecordController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,6 @@ Route::get('/', function () {
 });
 
 Route::resource('patients', PatientController::class);
+Route::get('patients/{patient}/medical-records/create', [MedicalRecordController::class, 'create'])->name('medical-records.create');
+Route::post('patients/{patient}/medical-records', [MedicalRecordController::class, 'store'])->name('medical-records.store');
+Route::get('patients/{patient}/medical-records/{record}', [MedicalRecordController::class, 'show'])->name('medical-records.show');
